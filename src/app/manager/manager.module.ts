3b31830f163cecc3d86e584dd48c1052309aff59
gunhant <34014347+gunhant@users.noreply.github.com>
2018-11-29 16:48:23 -0500

    Manager Module with SubModules

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { MaterialModule } from '../material.module';
+import { ManagerComponent } from './manager.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component';
 
 @NgModule({
-  declarations: [ManagerHomeComponent],
+  declarations: [ManagerHomeComponent, ManagerComponent, UserManagementComponent, ReceiptLookupComponent],
   imports: [
     CommonModule,
-    ManagerRoutingModule
+    ManagerRoutingModule,
+    MaterialModule,
+    ManagerComponent,
+    UserManagementComponent,
+    ReceiptLookupComponent
   ]
 })
 export class ManagerModule { }
