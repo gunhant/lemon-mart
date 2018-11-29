1f2259400880dc000f974ad5ef172de974dc2528
gunhant <34014347+gunhant@users.noreply.github.com>
2018-11-29 17:04:02 -0500

    Revert "Manager Module with SubModules"
    This reverts commit 3b31830f163cecc3d86e584dd48c1052309aff59.

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
-import { ManagerComponent } from './manager.component';
-import { UserManagementComponent } from './user-management/user-management.component';
-import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component';
 
 
 const routes: Routes = [
-  {
-    path: '', component: ManagerComponent, children: [
-      { path: '', redirectTo: '/manager/home', pathMatch: 'full' },
-      { path: 'home', component: ManagerHomeComponent },
-      { path: 'users', component: UserManagementComponent },
-      { path: 'receipts', component: ReceiptLookupComponent }
-    ]
-  }
+    { path: '', redirectTo: '/manager/home', pathMatch: 'full' },
+    { path: 'home', component: ManagerHomeComponent }
 ];
 
 @NgModule({
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -3,20 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
-import { MaterialModule } from '../material.module';
-import { ManagerComponent } from './manager.component';
-import { UserManagementComponent } from './user-management/user-management.component';
-import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component';
 
 @NgModule({
-  declarations: [ManagerHomeComponent, ManagerComponent, UserManagementComponent, ReceiptLookupComponent],
+  declarations: [ManagerHomeComponent],
   imports: [
     CommonModule,
-    ManagerRoutingModule,
-    MaterialModule,
-    ManagerComponent,
-    UserManagementComponent,
-    ReceiptLookupComponent
+    ManagerRoutingModule
   ]
 })
 export class ManagerModule { }
